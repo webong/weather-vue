8392784ac65c09ecc940f58d6109a998cf008322
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-20 19:13:03 +0100

    use env file

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  WEATHER_API: {
-    search: '"https://xeta.000webhostapp.com/weather.php?command=search&keyword="',
-    weather: '"https://xeta.000webhostapp.com/weather.php?command=location&woeid="'
-  }
+  WEATHER_API: 'http://xeta.000webhostapp.com/weather.php'
 }
