66aa274c19e3f9079e86b013bbac858e46d74bf6
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-20 19:18:12 +0100

    env file error

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  WEATHER_API: 'http://xeta.000webhostapp.com/weather.php' 
+  WEATHER_API: '"http://xeta.000webhostapp.com/weather.php"' 
 })
