09b576d1f79626db1100986c8cfab88d540814a0
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-20 19:26:55 +0100

    updated yarn.lock

--- location.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$woeid = $_GET['woeid'];
-//var_dump($woeid);
-$query = file_get_contents('${process.env.WEATHER_API}?command=location&woeid=' . $woeid );
-
-if($query){
-    return $query;
-}
