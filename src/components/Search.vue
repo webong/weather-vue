e94bd7bf0179e53222620846e2b573c8062590a4
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-20 01:01:24 +0100

    fox

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,22 +1,10 @@
 <template>
-  <div class="row">
-     <div class="col-md-4">
-        <div class="card mb-4 shadow-sm">
-          <img class="card-img-top" src="" data-src="" alt="Card image cap">
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-
-              </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
-        </div>
-     </div>
-  </div>
-          
+    <div>
+            <div class="h5">Showing Results for {{ $route.params.location }}</div>
+              <div class="row">
+                <Weather v-for="city in cities" :key="city.id"> </Weather>
+            </div> 
+    </div>       
 </template>
 
 <script>
@@ -24,16 +12,47 @@ import moment from 'moment'
 import 'moment-timezone'
 
 export default {
-    name: 'Weather',
+    name: 'Forecast',
     props: {
         
     },
+    created() {
+      console.log('route', this.$route.params.location);
+      console.log('app', this.$app)
+         fetch(`${process.env.API_URL.search}${query}`)
+                    .then(response => {
+                        if (response.status !== 200) {
+                            commit('setAppStatus', {
+                                state: 'error',
+                                message: 'Uh oh, the geolocation API is not responding. Please try again.'
+                            })
+                            return
+                        }
+                        response.json().then(data => {
+                            if (!data.length) {
+                                commit('setAppStatus', {
+                                    state: 'error',
+                                    message: 'No results found. Please try another search.'
+                                })
+                                return
+                            }
+                            commit('setGeocode', data[0])
+                            resolve(data[0])
+                        })
+                    })
+                    .catch(() => {
+                        commit('setAppStatus', {
+                            state: 'error',
+                            message: 'Uh oh, the geolocation API is not responding.'
+                        })
+                    })
+    },
     computed: {
-      store () {
-        return this.$store.state
+      app () {
+        return this.$app.state
       },
       daily () {
-        return this.$store.state.weather.consolidated_weather
+        return this.$app.state.weather.consolidated_weather
       }
     },
     methods: {
