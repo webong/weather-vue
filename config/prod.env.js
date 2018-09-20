18da01c1430ebfa816473738135090218e185667
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-20 19:19:19 +0100

    env file error

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  WEATHER_API: '"http://xeta.000webhostapp.com/weather.php"'
+  WEATHER_API: '"https://xeta.000webhostapp.com/weather.php"'
 }
